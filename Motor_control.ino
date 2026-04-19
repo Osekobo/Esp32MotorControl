b8f48fb9325ddf5da6ffd4bde6d112d226279938
Osekobo <collinsoseko2003@gmail.com>
2026-04-19 22:56:46 +0300

    First commit

--- Motor_control.ino
+++ Motor_control.ino
@@ -1,13 +1,9 @@
 #include <WiFi.h>
 #include <WebServer.h>
-
 const char* ssid = "MOOD";
 const char* password = "1password1";
-
 WebServer server(80);
-
 int motorPin = 2;
-
 void handleRoot() {
   String html = "<h1>ESP32 Motor Control</h1>";
   html += "<p><a href='/on'><button>ON</button></a></p>";
@@ -15,29 +11,22 @@ void handleRoot() {
   html += "<p>Motor is " + String(digitalRead(motorPin) ? "ON" : "OFF") + "</p>";
   server.send(200, "text/html", html);
 }
-
 void handleOn() {
   digitalWrite(motorPin, HIGH);
   server.sendHeader("Location", "/");
   server.send(303);
 }
-
 void handleOff() {
   digitalWrite(motorPin, LOW);
   server.sendHeader("Location", "/");
   server.send(303);
 }
-
 void setup() {
   Serial.begin(115200);
-
   pinMode(motorPin, OUTPUT);
   digitalWrite(motorPin, LOW);
-
   WiFi.begin(ssid, password);
-
   Serial.print("Connecting");
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
